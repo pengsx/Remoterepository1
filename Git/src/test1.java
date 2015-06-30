f2f2f731df42f20f9a558ef8123a60a3f33d3e47
Student <Student@III-EEIT>
2015-06-30 10:16:08 +0800

    01234567

---
+++ Git/src/test1.java
@@ -0,0 +1,10 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("0123456");
+		System.out.println("01234567");
+	}
+
+}
